fa5c59cb45d05e8a95907b6dc76dcde43b949f9f
gtabirta <ghenadie.tabirta@gmail.com>
2023-02-18 15:29:41 +0200

    actualizare

--- src/SorterApp.java
+++ src/SorterApp.java
@@ -8,7 +8,12 @@ public class SorterApp {
         sorter.increment(1000);
 
         //System.out.println(sorter.getNum());
+        System.out.println( "Numere din intervalul 100 - 1000 care se impart la 5:");
         System.out.println(sorter.getCollFive());
+        //System.out.println (sorter.getCollFive1 ());
+        //System.out.println (sorter.collFiveArr[5][5]);
+        //System.out.println( " ");
+        System.out.println( "Numere din intervalul 100 - 1000 care se impart la 6:");
         System.out.print(sorter.getCollSix());
         }
 }
--- src/sorter/Sorter.java
+++ src/sorter/Sorter.java
@@ -44,21 +44,75 @@ public class Sorter {
 
    private String collFive;
 
-   public void appendCollFive(int num) {
-      if (this.collFive == null)
-         this.collFive = String.valueOf(num);
-      else
-         this.collFive = collFive + " " + num;
+   public int collFiveArr [][] = new int [19][10];
+   public int[][] getCollFiveArr() {
+      return collFiveArr;
    }
-
+   int colNum = 0;
+   int rowNum = 0;
+   public void appendCollFiveArr(int num) {
+     // if (this.collFive == null)
+     //    this.collFive = String.valueOf(num);
+     // else
+     //   this.collFive = collFive + " " + num;
+         if (colNum != 9) {
+            //System.out.println (num);
+            //System.out.println (rowNum + " " + colNum);
+            this.collFiveArr [rowNum][colNum] = num;
+            colNum++;
+            //System.out.print (this.collFiveArr[rowNum][colNum]);
+         }
+         else {
+            //System.out.println (num);
+            //System.out.println (rowNum + " " + colNum);
+            this.collFiveArr [rowNum][colNum] = num;
+            colNum = 0;
+            rowNum++;
+            //System.out.print (this.collFiveArr[rowNum][colNum]);
+         }
+         collFiveArr = this.collFiveArr;
+      }
+      //System.out.print (collFive1[0][0]);
    public String getCollFive() {
-      return collFive;
+      {
+         for (int row = 0; row < getCollFiveArr().length; row++) {
+            for (int col = 0; col < getCollFiveArr()[row].length; col++) {
+               int numArr;
+               //System.out.println(row + " " + col);
+               numArr = getCollFiveArr()[row][col];
+               //System.out.println(numArr);
+               appendCollFive(numArr);
+               //System.out.println(collFive);
+
+            }
+         }
+      }
+      return this.collFive = collFive;
    }
 
-   public void setCollFive(String collFive) {
+   public String setCollFive() {
       this.collFive = collFive;
+      return this.collFive;
    }
-
+  int countFive = 1;
+   public void appendCollFive(int numArr) {
+         if (this.collFive == null) {
+            this.collFive = String.valueOf(numArr);
+            countFive++;
+         }
+         else if (this.collFive != null && countFive != 10 && countFive != 1) {
+            this.collFive = collFive + " " + String.valueOf(numArr);
+            countFive++;
+         }
+         else if (this.collFive != null && countFive == 10) {
+            this.collFive = collFive + " " + String.valueOf(numArr) + "\n";
+            countFive = 1;
+         }
+         else if (this.collFive != null && countFive == 1) {
+            this.collFive = collFive + String.valueOf(numArr);
+            countFive++;
+         }
+   }
    private String collSix;
 
    public String getCollSix() {
@@ -91,7 +145,7 @@ public class Sorter {
       public void distribute (int num){
          if (num > getMinNum() && num < getMaxNum()) {
             if (num % 5 == 0)
-               appendCollFive(num);
+               appendCollFiveArr(num);
             if (num % 6 == 0)
                appendCollSix(num);
          }
